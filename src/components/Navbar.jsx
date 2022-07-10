44e9f26e3012cd7b38bb3953e0a2df1b7bfd6c06
mrjaat-tech <ankurtehlan2000@gmail.com>
2022-07-10 23:01:39 +0530

    make some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import mypic from '../data/mypic.jpg';
+import mypic from '../data/newpic.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
